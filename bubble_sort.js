ab987d1ffae299e9364ce786e571fe8c71a609aa
Taron <79373250+taron0@users.noreply.github.com>
2021-07-09 01:00:27 +0400

    create bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -17,4 +17,21 @@ let bubbleSort = (inputArr) => {
 
 // or 
 
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    let checked;
+    do {
+        checked = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                checked = true;
+            }
+        }
+    } while (checked);
+    return inputArr;
+};
+
 
